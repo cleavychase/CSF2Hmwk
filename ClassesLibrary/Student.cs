c1667c958325801bc3df7117237fbb88493c1ff5
caitlin chase <caitleavy@gmail.com>
2021-05-26 13:12:24 -0500

    Finished Students

---
+++ ClassesLibrary/Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Student
+    {
+        //fields
+        private string _firstName;
+        private string _lastName;
+        private string _iD;
+        private float _gPA;
+        
+
+       
+        //properties
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public string ID
+        {
+            get { return _iD; }
+            set { _iD = value; }
+        }
+        public float GPA
+        {
+            get { return _gPA; }
+            set { _gPA = value; }
+        }
+        //ctors - Make a default ctor and FQCTOR
+        public Student(string firstName, string lastName, string iD, float gPA)
+        {
+            //Prop = param;
+            FirstName = firstName;
+            LastName = lastName;
+            ID = iD;
+            GPA = gPA;
+        }//end of Fully Qualified CTOR (FQCTOR) - we used every property in the creation of the object
+        public Student()
+        {
+
+        }
+        //methods
+        public override string ToString()
+        {
+            //return base.ToString();
+            return string.Format("First Name: {0}\n" +
+                "Last Name: {1}\n" +
+                "Student ID: {2}\n" +
+                "GPA {3}\n",
+                FirstName,
+                LastName,
+                ID,
+                GPA);
+        }
+        }
+}
---
+++ TesterProgram/TesterProgram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassesLibrary;
+
+namespace TesterProgram
+{
+    class TesterProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n------Student Info------\n\n");
+            
+            Student s1 = new Student();
+            s1.FirstName = "Caitlin";
+            s1.LastName = "Chase";
+            s1.ID = "1234";
+            s1.GPA = 4.03f;
+
+            
+            Console.WriteLine(s1);
+
+            Student s2 = new Student("John", "Batman", "0000", 2.5f);
+
+            Console.WriteLine(s2);
+        }
+    }
+}
